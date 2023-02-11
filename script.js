ef44c1a65afe3e23f9c41aace4e3d0e6efe77c17
Azralii <111491491+Azralii@users.noreply.github.com>
2023-02-11 22:23:39 +0100

    Jag gjorde Randon APOD som visar oss 10 bilder

--- script.js
+++ script.js
@@ -1,51 +1,57 @@
-// example fetch using pokemonapi.con
-document.querySelector("#search-button").addEventListener("click", getFetch)
+    // Selectors
+    const button = document.querySelector("#search-btn");
 
-function getFetch(){
-    const API_key = "5v0dtub4I4Aeci75CYMSVMIG9dRCrmnE3yVqkMoz";
-    const APOD_endpiont = `https://api.nasa.gov/planetary/apod?api_key=${API_key}`;
-    //const choice = document.querySelector("input").value
-    //console.log(choice)
+    //Event Listeners
+    button.addEventListener("click", getFetchApod);
+    button.addEventListener("click", getSearch);
 
-    fetch(APOD_endpiont)
-           .then(response => response.json()) // parse response as json
-           .then(data => {
-            console.log(data)
-            document.querySelector("#content-1").innerHTML += `<h2>Astronomy Picture of the Day</h2><h4 style="text-align: center">Date of the Picture: "${data.date}"</h4><br>`
-            document.querySelector("#content-1").innerHTML += `<p style="text-align: left">"${data.explanation}"</p><br>`
-            document.querySelector("#content-1").innerHTML += `<img src="${data.hdurl}" width="1242px">`
-           })
+    //Functions
+    function getFetchApod(){
+        const API_key = "5v0dtub4I4Aeci75CYMSVMIG9dRCrmnE3yVqkMoz";  // MY NASA API KEY
+        const APOD_endpiont = `https://api.nasa.gov/planetary/apod?api_key=${API_key}&count=10`; // NASA API APOD ENDPOINT
 
-           .catch(err => {
-            console.log(`error  ${err}`)
+        fetch(APOD_endpiont)
+            .then(response => response.json()) // parse response as json
+            .then(data => {
+                const randomResult = data;
+                for (let i=0; i < randomResult.length; i++)
+                {
+                    console.log(data)
+                    document.querySelector(".content-1").innerHTML += `<h2>Astronomy Picture of the Day</h2><h4 style="text-align: center">Date of the Picture: "${randomResult[i].date}"</h4><br>`
+                    document.querySelector(".content-1").innerHTML += `<p style="text-align: left">"${randomResult[i].explanation}"</p><br>`
+                    document.querySelector(".content-1").innerHTML += `<img src="${randomResult[i].hdurl}" width="780px">`
+                }
+                
+            })
 
-           });
-           const search_endpoint = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${API_key}`;
-           fetch(search_endpoint)
-           .then(response => response.json()) // parse response as json
-           .then(data => {
-            console.log(data)
-            document.querySelector("#content-2").innerHTML += `<h2>Mars Rover Photos</h2><br>`
-            //document.querySelector("#content-2").innerHTML += `<p style="text-align: left">"${data.explanation}"</p><br>`
-            document.querySelector("#content-2").innerHTML += data.photos[0]
-           })
+            .catch(err => {
+                console.log(`error  ${err}`)
 
-           .catch(err => {
-            console.log(`error  ${err}`)
-
-           });
-}
+            });
+    }
 
 // om Nasa Image Video endpiont...
-let url  = "https://images-api.nasa.gov/search?q=space";
-const button = document.querySelector("#search-button");
-button.addEventListener("click", () => {
-    const search = document.querySelector("#search");
+    async function getSearch(){
+            const search = document.querySelector("input");
+            let url  = `https://images-api.nasa.gov/search?q=${search.value}`; // NASA API for IMAGE AND VIDEO ENDPOINT
 
-    url  = `https://images-api.nasa.gov/search?q=${search.value}`
-    fetch(url).then(response => response.json()).then((data) => {
-        console.log("data", data);
-
-    })
-});
+            let response = await fetch(url);
+            let data = await response.json()
+            console.log("Search data", data);
+            searchApiData(data);
+            
+    }
+    function searchApiData(data){
+            let filteredArray = data.collection.items.filter(item => item.data[0].media_type==='image'); // Filtering the fetched result
+            for(let i=0; i<filteredArray.length; i++)
+            {
+                console.log("Filtered Array", filteredArray[i].data);
+                document.querySelector("#search-content").innerHTML += `<h1>Title: "${filteredArray[i].data[0].title}"</h1>`
+                document.querySelector("#search-content").innerHTML += `<h3>Date Created: "${filteredArray[i].data[0].date_created}"</h3>`
+                document.querySelector("#search-content").innerHTML += `<h4>Center: "${filteredArray[i].data[0].center}"</h4>`
+                document.querySelector("#search-content").innerHTML += `<p>Description: "${filteredArray[i].data[0].description}"</p>`
+                document.querySelector("#search-content").innerHTML += `<img src="${filteredArray[i].href[0]}" width="1242px">`
+            }
+    }
+    
 
