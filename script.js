557ee6ac8e126cc37762cb16f28860b6a7d7332c
Azralii <111491491+Azralii@users.noreply.github.com>
2023-02-13 10:12:00 +0100

    jag är klart

--- script.js
+++ script.js
@@ -16,10 +16,9 @@
                 const randomResult = data;
                 for (let i=0; i < randomResult.length; i++)
                 {
-                    console.log(data)
                     document.querySelector(".content-1").innerHTML += `<h2>Astronomy Picture of the Day</h2><h4 style="text-align: center">Date of the Picture: "${randomResult[i].date}"</h4><br>`
                     document.querySelector(".content-1").innerHTML += `<p style="text-align: left">"${randomResult[i].explanation}"</p><br>`
-                    document.querySelector(".content-1").innerHTML += `<img src="${randomResult[i].hdurl}" width="780px">`
+                    document.querySelector(".content-1").innerHTML += `<img src="${randomResult[i].hdurl}" width="840px">`
                 }
                 
             })
@@ -32,16 +31,26 @@
 
 // om Nasa Image Video endpiont...
     async function getSearch(){
+            const imageCheckBox = document.getElementById("image-btn");
+            const videoCheckBox = document.getElementById("video-btn");
             const search = document.querySelector("input");
             let url  = `https://images-api.nasa.gov/search?q=${search.value}`; // NASA API for IMAGE AND VIDEO ENDPOINT
 
             let response = await fetch(url);
             let data = await response.json()
             console.log("Search data", data);
-            searchApiData(data);
+            if(imageCheckBox.checked == true){
+                imageSearchApiData(data);
+            }
+            else if(videoCheckBox.checked == true) {
+                videoSearchApiData(data);
+            }
+            else {
+                document.querySelector("#search-content").innerHTML += `<h1>No Data Found!!! Please Check that you checked the checkbox. </h1>`
+            }
             
     }
-    function searchApiData(data){
+    function imageSearchApiData(data){
             let filteredArray = data.collection.items.filter(item => item.data[0].media_type==='image'); // Filtering the fetched result
             for(let i=0; i<filteredArray.length; i++)
             {
@@ -50,8 +59,20 @@
                 document.querySelector("#search-content").innerHTML += `<h3>Date Created: "${filteredArray[i].data[0].date_created}"</h3>`
                 document.querySelector("#search-content").innerHTML += `<h4>Center: "${filteredArray[i].data[0].center}"</h4>`
                 document.querySelector("#search-content").innerHTML += `<p>Description: "${filteredArray[i].data[0].description}"</p>`
-                document.querySelector("#search-content").innerHTML += `<img src="${filteredArray[i].href[0]}" width="1242px">`
+                document.querySelector("#search-content").innerHTML += `<img src="${filteredArray[i].links[0].href}" width="840px">`
             }
     }
+    function videoSearchApiData(data){
+        let filteredArray = data.collection.items.filter(item => item.data[0].media_type==='video'); // Filtering the fetched result
+        for(let i=0; i<filteredArray.length; i++)
+        {
+            console.log("Filtered Array", filteredArray[i].data);
+            document.querySelector("#search-content").innerHTML += `<h1>Title: "${filteredArray[i].data[0].title}"</h1>`
+            document.querySelector("#search-content").innerHTML += `<h3>Date Created: "${filteredArray[i].data[0].date_created}"</h3>`
+            document.querySelector("#search-content").innerHTML += `<h4>Center: "${filteredArray[i].data[0].center}"</h4>`
+            document.querySelector("#search-content").innerHTML += `<p>Description: "${filteredArray[i].data[0].description}"</p>`
+            document.querySelector("#search-content").innerHTML += `<iframe src="${filteredArray[i].links[0].href}"></iframe>`
+        }
+}
     
 
